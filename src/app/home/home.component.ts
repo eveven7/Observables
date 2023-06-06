2fb150c5194068abca52ce59e18302c5ce2dcb08
Evelina <evelina.venckute@swedbank.lt>
2023-06-06 11:48:36 +0300

    map operator

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription, interval } from 'rxjs';
 import { Observable } from 'rxjs-compat';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -28,18 +29,25 @@ export class HomeComponent implements OnInit, OnDestroy {
         count++;
       }, 1000);
     });
-    this.firstObsSubscription = customIntervalObservable.subscribe(
-      (data) => {
-        console.log(data);
-      },
-      (error) => {
-        console.log(error);
-        alert(error.message);
-      },
-      () => {
-        console.log('completed');
-      }
-    );
+
+    this.firstObsSubscription = customIntervalObservable
+      .pipe(
+        map((data: number) => {
+          return 'round' + (data + 1);
+        })
+      )
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          console.log('completed');
+        }
+      );
   }
   ngOnDestroy(): void {
     this.firstObsSubscription.unsubscribe();
